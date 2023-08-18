3147f610ed21d3bb37823f5286ac43881fc934c0
rsyncOSX <thomeven@gmail.com>
2023-08-18 15:55:44 +0200

    Initial Commit

---
+++ RsyncUIHTML/RsyncUIHTMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  RsyncUIHTMLApp.swift
+//  RsyncUIHTML
+//
+//  Created by Thomas Evensen on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct RsyncUIHTMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
