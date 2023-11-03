3795968c8eef6069f9318a7c70b73aaf23fc43d3
rsyncOSX <thomeven@gmail.com>
2023-11-03 10:00:51 +0100

    Update

--- RsyncUIHTML/ContentView.swift
+++ RsyncUIHTML/ContentView.swift
@@ -38,7 +38,7 @@ struct ContentView: View {
         .toolbar(content: {
             ToolbarItem {
                 guipicker
-                    .tooltip("Select RSSfeed")
+                    .help("Select RSSfeed")
             }
 
         })
@@ -65,56 +65,5 @@ struct ContentView: View {
                 await rssfeed.seturl(selectedgui)
             }
         }
-    }
-}
-
-extension View {
-    func tooltip(_ tip: String) -> some View {
-        ZStack {
-            background(GeometryReader { childGeometry in
-                TooltipView(tip, geometry: childGeometry) {
-                    self
-                }
-            })
-            self
-        }
-    }
-}
-
-struct TooltipView<Content>: View where Content: View {
-    let content: () -> Content
-    let tip: String
-    let geometry: GeometryProxy
-
-    init(_ tip: String, geometry: GeometryProxy, @ViewBuilder content: @escaping () -> Content) {
-        self.content = content
-        self.tip = tip
-        self.geometry = geometry
-    }
-
-    var body: some View {
-        Tooltip(tip, content: content)
-            .frame(width: geometry.size.width, height: geometry.size.height)
-    }
-}
-
-struct Tooltip<Content: View>: NSViewRepresentable {
-    typealias NSViewType = NSHostingView<Content>
-
-    init(_ text: String?, @ViewBuilder content: () -> Content) {
-        self.text = text
-        self.content = content()
-    }
-
-    let text: String?
-    let content: Content
-
-    func makeNSView(context _: Context) -> NSHostingView<Content> {
-        NSViewType(rootView: content)
-    }
-
-    func updateNSView(_ nsView: NSHostingView<Content>, context _: Context) {
-        nsView.rootView = content
-        nsView.toolTip = text
     }
 }
